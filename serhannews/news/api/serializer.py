bfe1a6c2b2e5f4644686c833a45afdbef7957256
Serhan Elmalı <serhanelmali@gmail.com>
2022-10-22 12:27:36 +0300

    adds Article serializer

--- serhannews/news/api/serializer.py
+++ serhannews/news/api/serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-
+from news.models import Article
 
 
 
 class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     author = serializers.CharField()
     title = serializers.CharField()
     description = serializers.CharField()
@@ -13,3 +14,18 @@ class ArticleSerializer(serializers.Serializer):
     active = serializers.BooleanField()
     create_date = serializers.DateTimeField(read_only=True)
     update_date = serializers.DateTimeField(ready_only=True)
+
+    def create(self, validated_data):
+        print(validated_data)
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.author = validated_data.get('author', instance.author)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.text = validated_data.get('text', instance.text)
+        instance.city = validated_data.get('city', instance.city)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
