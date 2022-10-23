a42fade68c53f51317d410b3d1318f9c8208df88
Serhan Elmalı <serhanelmali@gmail.com>
2022-10-24 00:32:54 +0300

    refactors serializer to modelserializer

--- serhannews/news/api/serializers.py
+++ serhannews/news/api/serializers.py
@@ -1,19 +1,27 @@
 from rest_framework import serializers
 from news.models import Article
 
+from datetime import datetime
+from django.utils.timesince import timesince
 
+class ArticleSerializer(serializers.ModelSerializer):
+    time_since_release = serializers.SerializerMethodField()
+    class Meta:
+        model = Article
+        fields = '__all__'
+        read_only_fields = ['id', 'release_date', 'update_date']
+    
+    def get_time_since_release(self, object):
+        now = datetime.now()
+        release_date = object.release_date
+        time_delta = timesince(release_date, now)
+        return time_delta
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    text = serializers.CharField()
-    city = serializers.CharField()
-    release_date = serializers.DateField()
-    active = serializers.BooleanField()
-    create_date = serializers.DateTimeField(read_only=True)
-    update_date = serializers.DateTimeField(read_only=True)
+    def validate_release_date(self, value):
+        today = datetime.today()
+        if value > today:
+            raise serializers.ValidationError("Release date can't be future!")
+        return value
 
     def create(self, validated_data):
         print(validated_data)
