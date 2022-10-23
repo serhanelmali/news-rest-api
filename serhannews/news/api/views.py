dbb907f6fe3824848b5d1f29074a47ca6fd7ee85
Serhan Elmalı <serhanelmali@gmail.com>
2022-10-23 23:50:46 +0300

    adds article detail api view

--- serhannews/news/api/views.py
+++ serhannews/news/api/views.py
@@ -20,3 +20,41 @@ def article_list_create_api_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail_api_view(request, pk):
+    try:
+        article_instance = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        {
+            'errors':{
+                'code': 404,
+                'message': f'The article ({pk}) does not exists.'
+            }
+        },
+        Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article_instance)
+        return Response(serializer.data)
+
+
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article_instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        article_instance.delete()
+        return Response(
+            {        
+                'errors':{
+                    'code': 204,
+                    'message': f'The article ({pk}) is deleted.'
+                }
+            },
+       status=status.HTTP_204_NO_CONTENT
+       )
